103d69166795c51bb83dd0fbdd75c9b1331cd553
Ashutosh <mishra.techie@gmail.com>
2016-08-30 21:43:34 +0530

    Initial commit

---
+++ src/test/java/TestClass.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+
+/**
+ * Created by amishra4 on 8/30/2016.
+ */
+
+public class TestClass {
+
+
+    @org.junit.Test
+    public void testme(){
+
+        System.out.println(System.getProperty("propertyName"));
+        System.out.println(System.getProperty("browser"));
+        System.out.println(System.getProperty("browserA"));
+    }
+
+
+
+
+}
